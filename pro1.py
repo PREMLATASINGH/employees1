9c8bb278fff31ccee1d159b2674d3d02f2af6a9b
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-08-11 14:39:22 -0400

    first commit

---
+++ pro1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+data={
+    'emp_id':[101,102,103,104,105,106,107,108,109,110],
+    'emp_name':['John','Alice','Bob','Eve','Charlie','David','Frank','Grace','Hannah','Ivy'],
+    'emp_salary':[50000,60000,55000,70000,65000,55000,60000,70000,55000,65000] , 
+    'department':['HR','Finance','IT','Marketing','Sales','IT','Finance','HR','Marketing','Sales']  
+}
+df=pd.DataFrame(data)
+print(df)
