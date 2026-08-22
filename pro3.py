4d5954f3b366322ab652bc994f2cf49f308ee1ab
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-08-22 10:08:14 -0400

    new table

---
+++ pro3.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+data={
+    'order_id':[101,102,103,104,105,106,107,108,109,110],
+    'customer_id':[1,2,3,4,5,6,7,8,9,10],
+    'order_amount':[250,300,150,400,350,200,450,500,600,700],
+    'order_date':['2023-01-01','2023-01-02','2023-01-03','2023-01-04','2023-01-05','2023-01-06','2023-01-07','2023-01-08','2023-01-09','2023-01-10'],
+    'order_status':['Completed','Pending','Completed','Cancelled','Completed','Pending','Completed','Completed','Cancelled','Completed']
+,
+'sales_region':['North','South','East','West','North','South','East','West','North','South'],
+'product_category':['Electronics','Clothing','Home','Beauty','Electronics','Clothing','Home','Beauty','Electronics','Clothing'] ,
+'price':[100,50,200,150,300,250,400,350,500,450],
+'quantity':[2,3,1,4,2,5,3,2,1,4],
+'discount':[10,5,20,15,30,25,40,35,50,45],
+'tax':[5,2.5,10,7.5,15,12.5,20,17.5,25,22.5],
+'payment_method':['Credit Card','PayPal','Credit Card','Debit Card','PayPal','Credit Card','Debit Card','PayPal','Credit Card','Debit Card'],
+'delivery_time':[2,3,1,4,2,5,3,2,1,4],
+'customer_feedback':['Good','Average','Excellent','Poor','Good','Average','Excellent','Poor','Good','Average'],
+}
+df=pd.DataFrame(data)
+print(df)
